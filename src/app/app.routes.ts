e89ab0f197b12913f5ceae96a1551b09c5a07c1a
Tiago Machado <tiago.machado.1987@gmail.com>
2024-07-15 18:51:31 +0200

    add pages/components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,32 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+//COMPONENTS
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { CatalogComponent } from './pages/catalog/catalog.component';
+
+export const routes: Routes = [
+    {
+        path: 'home',
+        component: HomePageComponent
+    },
+    {
+        path: 'about',
+        component: AboutComponent
+    },
+    {
+        path: 'contact',
+        component: ContactComponent
+    },
+    {
+        path: '',
+        component: CatalogComponent
+    },
+    /*
+    {
+        path: 'product/:productId',
+        component: ProductsComponent
+    },
+    */
+  ]
